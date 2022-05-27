22121d9aaaf3e4bb37ba73983dcf0bca1f41a5ee
Ankit-Cha <ankitcha1998.ac@gmail.com>
2022-05-27 21:29:23 +0530

    complain activity

---
+++ AGA/app/src/main/java/com/example/apnagharashram/issue_complain.java
@@ -0,0 +1,14 @@
+package com.example.apnagharashram;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class issue_complain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_issue_complain);
+    }
+}
